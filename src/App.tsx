21c2d771fd566494044110ecc09e18707eb9c5ca
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2024-01-17 13:13:44 +0530

    Supply working

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,16 @@ import { BigNumber, ethers } from "ethers";
 import { Pool } from "@aave/contract-helpers";
 
 function App() {
-  // const provider = new ethers.providers.AlchemyProvider(
-  //   "goerli",
-  //   ""//I have given my acccess key
-  // );
-  // const network = ethers.providers.getNetwork("sepolia");
-  // const provider = new ethers.providers.EtherscanProvider(network);
-  // console.log(provider);
-
-  const provider = new ethers.providers.InfuraProvider("sepolia");
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const connectwalletHandler = () => {
+    if (window.ethereum) {
+      provider.send("eth_requestAccounts", []).then(async () => {
+        console.log("Connected");
+      });
+    } else {
+    }
+  };
+  connectwalletHandler();
   const pool = new Pool(provider, {
     POOL: "0x6Ae43d3271ff6888e7Fc43Fd7321a503ff738951",
     WETH_GATEWAY: "0xDde0E8E6d3653614878Bf5009EDC317BC129fE2F",
@@ -40,8 +41,7 @@ function App() {
       for (const tx of txs) {
         const extendedTxData = await tx.tx();
         const { from, ...txData } = extendedTxData;
-        const signer = new ethers.Wallet("PrivateKey", provider);
-        // const signer = provider.getSigner(from);
+        const signer = provider.getSigner(from);
         const txResponse = await signer.sendTransaction({
           ...txData,
           value: txData.value ? BigNumber.from(txData.value) : undefined,
@@ -60,7 +60,7 @@ function App() {
           submitTransaction({
             user: "0x4b4b30e2E7c6463b03CdFFD6c42329D357205334",
             reserve: "0x94a9D9AC8a22534E3FaCa9F4e7F2E2cf85d5E4C8",
-            amount: "10000",
+            amount: "100",
             onBehalfOf: "0x4b4b30e2E7c6463b03CdFFD6c42329D357205334",
           })
         }
